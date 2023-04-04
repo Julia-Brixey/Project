2e011552b0f22e5ae5fe5368f34a13e5dbce1a88
Julia-Brixey <juliabrixey23@gmail.com>
2023-04-04 13:13:45 -0500

    Using GPU

--- Main1.py
+++ Main1.py
@@ -9,14 +9,20 @@ from ResNet import ResNet, Block, Bottleneck
 
 def train():
 
+    # OTU Dataset Labels: Healthy Controls (0), Major Depressive Disorder (1), Schizophrenia (2)
+
     # Julia's Mac
     # dataset = LoadDataset(r"/Users/juliabrixey/Desktop/Research/Honors Thesis/Project/data/OTU")
 
     # Julia's PC
-    dataset = LoadDataset(r"C:\Users\jkbrixey\Desktop\Honors Thesis\Project\data\OTU")
+    # dataset = LoadDataset(r"C:\Users\jkbrixey\Desktop\Honors Thesis\Project\data\OTU")
 
     # Ubuntu Remote server
-    # dataset = LoadDataset(r"C:\Users\jkbrixey\Desktop\Honors Thesis\Project\data\OTU")
+    dataset = LoadDataset(r"/home/jkbrixey/Project/Project/data/OTU")
+
+    # check if gpu is available and set device to cuda else cpu
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    print("device available is", device)
 
     dataset_size = len(dataset)
     indices = list(range(dataset_size))
@@ -28,30 +34,34 @@ def train():
     train_sample = SubsetRandomSampler(train_indices)
     test_sample = SubsetRandomSampler(test_indices)
 
-    train_loader = DataLoader(dataset, batch_size=10, sampler=train_sample)
-    test_loader = DataLoader(dataset, batch_size=10, shuffle=False, sampler=test_sample)
+    train_loader = DataLoader(dataset, batch_size=5, sampler=train_sample)
+    test_loader = DataLoader(dataset, batch_size=5, shuffle=False, sampler=test_sample)
 
     # UTO center has 3 classes
     num_classes = 3
     model = ResNet(layers=[2, 2, 2, 2], block=Block, num_classes=num_classes)
+    model = model.to(device)
 
     # Loss Function / Optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    # optimizer = optimizer.to(device)
 
     # Train
     num_epochs = 10
     for epoch in range(num_epochs):
         running_loss = 0.0
+        print("Epoch running ", epoch)
         for i, data in enumerate(train_loader, 0):
             inputs, labels = data
+            inputs.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-            if i % 100 == 99:
+            if i % 50 == 49:
                 print('[%d, %5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss / 100))
                 running_loss = 0.0
